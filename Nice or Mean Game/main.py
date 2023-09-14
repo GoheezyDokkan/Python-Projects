32d91d2c6cd4f5448ef82c5e9ebeea1623b76cf1
GoheezyDokkan <134324302+GoheezyDokkan@users.noreply.github.com>
2023-09-13 23:06:37 -0400

    Add files via upload

--- Nice or Mean Game/main.py
+++ Nice or Mean Game/main.py
@@ -77,6 +77,8 @@ def win(nice,mean,name):
 def lose(nice,mean,name):
     # Substitute the {} wildcards with our variable values
     print("\nAhhh too bad, game over! \n{}, you live in a dirty beat-up \nvan by the river, wretched and alone!".format(name))
+    # call again function and pass in our variables
+    again(nice,mean,name)
 
 def again(nice,mean,name):
     stop = True
